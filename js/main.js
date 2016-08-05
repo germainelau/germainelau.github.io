6cdd9835aaac25de9b057c030b463464b319edea
germainelau <germainelau@ymail.com>
2016-08-04 21:55:45 -0700

    Update: Photography Scroll

--- js/main.js
+++ js/main.js
@@ -18,64 +18,70 @@ $(window).on('scroll', function() {
 
 /* View Options ======================== */
 
-// If click .one add class #view-option 
-// Remove class #view-option from .four 
+$(document).ready(function(){
+    
+    $("#four").click(function(){
+    	$(".page").addClass("gridTile");
+    	$("#four").addClass("black");
+    	$("#one").removeClass("black");
+    });
+    $("#one").click(function(){
+    	$(".page").removeClass("gridTile");
+    	 $("#four").removeClass("black");
+    	$("#one").addClass("black");
+    });
 
-$('.one').on('click', function () {
-	$('.one').addClass('view-option');
-	$('.one').find('p').addClass('view-option');
-	$('.four').removeClass('view-option');
-	$('.four').find('p').removeClass('view-option');
-});
-
-// If click .four add class #view-option 
-// Remove class #view-option from .one 
-
-$('.four').on('click', function () {
-	$('.one').removeClass('view-option');
-	$('.one').removeClass('black');
-	$('.one').find('p').removeClass('view-option');
-	$('.four').addClass('view-option');
-	$('.four').find('p').addClass('view-option');
-});
-
-// If click .four add class #second-image-view to #second-image
-// If click .four add class #third-image-view to #third-image
-
-$('.four').on('click', function () {
-	$('#fullpage').empty();
-	$('#fullpage').html("<div class='section active'><img src='images/image_placeholder_1.jpg'></div><div class='section'><img src='images/image_placeholder_2.jpg' class='second-image '><img src='images/image_placeholder_1.jpg' class='third-image'><img src='images/image_placeholder_2.jpg' class='fourth-image'><img src='images/image_placeholder_1.jpg' class='fifth-image'></div>");
-	$('.second-image').addClass('second-image-view');
-	$('.third-image').addClass('third-image-view');
-	$('.fourth-image').addClass('fourth-image-view');
-	$('.fifth-image').addClass('fifth-image-view');
-});
-
-$('.one').on('click', function () {
-	$('.second-image').removeClass('second-image-view');
-	$('.third-image').removeClass('third-image-view');
-	$('.fourth-image').removeClass('fourth-image-view');
-	$('.fifth-image').removeClass('fifth-image-view');
-	$('#fullpage').empty();
-	$('#fullpage').html("<div id='fullpage'><div class='section active'><img src='images/image_placeholder_1.jpg'></div><div class='section'><img src='images/image_placeholder_2.jpg' class='second-image'></div><div class='section'><img src='images/image_placeholder_1.jpg' class='third-image'></div><div class='section'><img src='images/image_placeholder_2.jpg' class='fourth-image'></div><div class='section'><img src='images/image_placeholder_1.jpg' class='fifth-image'></div></div>");
 });
 
 /* Scroll ======================== */
 
-// var ypos,image;
-// function parallex () {
-// 	ypos = window.pageYOffset;
-// 	image = document.getElementById('image');
-// 	image.style.top = ypos * .4 + 'px';
-// }
-// window.addEventListener('scroll',parallex)
+var amount = '';
 
-$(document).ready(function() {
-    $('#fullpage').fullpage({
-    	scrollingSpeed: 600,
-    	autoScrolling: true,
-    	keyboardScrolling: true,
+function scroll() {
+    $('#gallery_container').animate({
+        scrollTop: amount
+    }, 100, 'linear',function() {
+        if (amount != '') {
+            scroll();
+        }
     });
+}
+$('#up').hover(function() {
+    amount = '+=32';
+    scroll();
+}, function() {
+    amount = '';
+});
+$('#down').hover(function() {
+    amount = '-=32';
+    scroll();
+}, function() {
+    amount = '';
 });
 
+/* Scroll ======================== */
+
+var amount = '';
+
+function scroll() {
+    $('#gallery_container_two').animate({
+        scrollTop: amount
+    }, 100, 'linear',function() {
+        if (amount != '') {
+            scroll();
+        }
+    });
+}
+$('#up_2').hover(function() {
+    amount = '+=30';
+    scroll();
+}, function() {
+    amount = '';
+});
+$('#down_2').hover(function() {
+    amount = '-=30';
+    scroll();
+}, function() {
+    amount = '';
+});
 
