3c841a59b8d9d7f53a5cd1fe541b199ea0aa4866
germainelau <germainelau@ymail.com>
2016-08-08 11:09:19 -0700

    Update: New Home Header

--- js/main.js
+++ js/main.js
@@ -86,6 +86,7 @@ $('.HomeScrollUp').hover(function(){
 	return false;
 });
 
+
 /* Collage ======================== */
 $(document).ready(function(){
 
@@ -93,9 +94,9 @@ $(document).ready(function(){
 		$("#collage").removeClass("grey")
 		$("#collage").addClass("black")
 		$("#grid").addClass("grey");
-		$('.gallery_two').html('<img src="images/PhotoDoll.jpg" alt=""><img src="images/PhotorebeccaVertical.jpg" alt=""><img src="images/PhotoTree copy.jpg" alt="">');
+		$('.gallery_two').html('<img src="images/PhotoDoll.jpg" alt="" class="doll"><img src="images/PhotorebeccaVertical.jpg" alt=""><img src="images/PhotoTree copy.jpg" alt="">');
 		$('.gallery_two').addClass('viewThreeMid');
-		$('.gallery_one').html('<img src="images/PhotoTwoLegs.jpg" alt=""><img src="images/PhotoBlueBuilding.jpg" alt=""><img src="images/PhotoPink.jpg" alt=""><img src="images/PhotoRebeccaEdit.jpg" alt="">');
+		$('.gallery_one').html('<img src="images/PhotoTwoLegs.jpg" alt=""><img src="images/PhotoBlueBuilding.jpg" alt="" class="blueBuilding"><img src="images/PhotoPink.jpg" alt=""><img src="images/PhotoRebeccaEdit.jpg" alt="">');
 		$('.gallery_one').addClass('viewThreeLarge');
 		$('.gallery_three').removeClass('hide');
 		$('.gallery_three').html('<img src="images/PhotoBuildings.jpg" alt=""><img src="images/PhotoKevin.jpg" alt=""><img src="images/PhotoLegs.jpg" alt=""><img src="images/PhotoPortrait.jpg" alt="">');
@@ -108,39 +109,33 @@ $(document).ready(function(){
 		$("#grid").addClass("black");
 		$("#collage").addClass("grey");
 		$('.gallery_two').removeClass('viewThreeMid');
-		$('.gallery_two').html('<img src="images/PhotoDoll.jpg" alt="" ><img src="images/PhotoLegs.jpg" alt=""><img src="images/PhotoKevin.jpg" alt=""><img src="images/PhotorebeccaVertical.jpg" alt=""><img src="images/PhotoPortrait.jpg" alt="">');
+		$('.gallery_two').html('<img src="images/PhotoDoll.jpg" alt="" class="doll"><img src="images/PhotoLegs.jpg" alt=""><img src="images/PhotoKevin.jpg" alt=""><img src="images/PhotorebeccaVertical.jpg" alt=""><img src="images/PhotoPortrait.jpg" alt="">');
 		$('.gallery_one').removeClass('viewThreeLarge');
-		$('.gallery_one').html('<img src="images/PhotoTwoLegs.jpg" alt=""><img src="images/PhotoBlueBuilding.jpg" alt=""><img src="images/PhotoRebeccaEdit.jpg" alt=""><img src="images/PhotoBuildings.jpg" alt=""><img src="images/PhotoPink.jpg" alt=""><img src="images/PhotoTree copy.jpg" alt="">');
-		$('.gallery_three').empty()	
-
-});	
+		$('.gallery_one').html('<img src="images/PhotoTwoLegs.jpg" alt=""><img src="images/PhotoBlueBuilding.jpg" alt="" class="blueBuilding"><img src="images/PhotoRebeccaEdit.jpg" alt=""><img src="images/PhotoBuildings.jpg" alt=""><img src="images/PhotoPink.jpg" alt=""><img src="images/PhotoTree copy.jpg" alt="">');
+		$('.gallery_three').empty();	
 
 });
 
+});
 
 /* Hover Descriptions ======================== */
 
 $(".blueBuilding").on('mouseenter',function(){
-	$('article').html('<div class="type"> <h3> Type: </h3><p>Photography</p></div><div class="dateEdit"><h3>View:</h3><span id="grid"> <i class="fa fa-th-large" aria-hidden="true"></i></span><span id="slashEdit">/</span><span id="collage"> <i class="fa fa-th" aria-hidden="true"></i></span> </div><div class="description text"><h3>Description:</h3><p>Financial District, 2016</p></div>');
-	$('article').hide();
-	$('article').fadeIn(200);
+	$('.description').find('p').html('<p>Financial district, 2016</p>');
+
 });
 
 $(".blueBuilding").on('mouseleave',function(){
-	$('article').html('<div class="type"> <h3> Type: </h3><p>Photography</p></div><div class="dateEdit"><h3>View:</h3><span id="grid"> <i class="fa fa-th-large" aria-hidden="true"></i></span><span id="slashEdit">/</span><span id="collage"> <i class="fa fa-th" aria-hidden="true"></i></span> </div><div class="description text"><h3>Description:</h3><p>From legs to skyscrapers&mdash; a collection of photos.</p></div>');
-	$('article').hide();
-	$('article').fadeIn(200);
+	$('.description').find('p').html('<p>From legs to skyscrapers&mdash; a collection of photos. </p>');
+
 });
 
 $(".doll").on('mouseenter',function(){
-	$('article').html('<div class="type"> <h3> Type: </h3><p>Photography</p></div><div class="dateEdit"><h3>View:</h3><span id="grid"> <i class="fa fa-th-large" aria-hidden="true"></i></span><span id="slashEdit">/</span><span id="collage"> <i class="fa fa-th" aria-hidden="true"></i></span> </div><div class="description text"><h3>Description:</h3><p>Floating doll head, 2015</p></div>');
-	$('article').hide();
-	$('article').fadeIn(200);
+	$('.description').find('p').html('<p>Floating doll head, 2015</p>');
+
 });
 
 $(".doll").on('mouseleave',function(){
-	$('article').html('<div class="type"> <h3> Type: </h3><p>Photography</p></div><div class="dateEdit"><h3>View:</h3><span id="grid"> <i class="fa fa-th-large" aria-hidden="true"></i></span><span id="slashEdit">/</span><span id="collage"> <i class="fa fa-th" aria-hidden="true"></i></span> </div><div class="description text"><h3>Description:</h3><p>From legs to skyscrapers&mdash; a collection of photos.</p></div>');
-	$('article').hide();
-	$('article').fadeIn(200);
-});
+	$('.description').find('p').find('p').html('<p>From legs to skyscrapers&mdash; a collection of photos. </p>');
 
+});
